8c1b9ae7057215e7d484510edf933688d9f04c8a
vlspriya <maddhap3@gmail.com>
2023-12-04 14:01:33 +0200

    lab done

---
+++ src/Toolbar.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Toolbar from './Toolbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reading Props in Event Handlers</h1>
+      <Toolbar />
+    </div>
+  );
+}
+
+export default App;
